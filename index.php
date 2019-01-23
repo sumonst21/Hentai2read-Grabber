c014fe72d90b82c551e61c1091d356bd1271291b
sinkaroid <bibsindra@gmail.com>
2019-01-24 03:24:27 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<title>Hentai2Read Stealer mirror</title>
+<link rel="shortcut icon" href="https://4.bp.blogspot.com/-DweioerHPNg/XEAf4rEqqqI/AAAAAAAABLc/d_BTxB3uqro1gxaU-iSgK0Apu3YA7pMfgCLcBGAs/s1600/slemanb0ys-ico.jpg" type="image/x-icon">
+<meta name='author' content='Sinkaroid Stupidc0de'>
+<link href="http://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet" type="text/css">
+
+
+<center><font face="Ubuntu" size="5" color="green"><b>[!] Hentai2Read Mirror [!]<b/></font></center>
+<br>
+<body bgcolor ="black">
+<center><font face="Ubuntu" size="4" color="green"><b><i>a sec till complete.. then, go put in hentai2read grabber</i><b/></font></center>
+<center><a href="grab.php" rel="nofollow" target="_blank"><font face="Ubuntu" size="4" color="white"><b>[ grab here ]<b/></font></a><center>
+<p>
+<center>
+<?php
+for ($i= 1; $i <= 38; $i++) 
+{
+echo" <img width='220' height='350' src=";
+/**geturl**/ echo "https://static.hentaicdn.com/hentai/26126/1"; ///Hentai2Read are different afterall, check your url seriously, may can [i/i2] subdomain galleries then look the folder after subdomain 'i/i2' it can be located in [001/002]
+echo "/ccdn000"; 
+echo $i; echo".jpg"; 
+echo ">";
+}
+
+
+
+?>
+<p>
+<center><font face="Ubuntu" size="5" color="green"><b>[!] Hentai2Read Stealer [!]<b/></font></center>
+<br>
+<center><font face="Ubuntu" size="3" color="green"><b><i>(c) 2019 sinkaroid ~penjagabisu@outlook.com</i><b/></font></center>
